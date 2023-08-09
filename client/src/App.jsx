e43fd788a3d5cd85dfeae9301648e3855d3e9bdc
parkhyeonji <iamphj3@naver.com>
2023-08-09 17:02:15 +0900

    chore : styled-components 설치

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
+import { styled } from 'styled-components';
+
 function App() {
   return (
-    <>
+    <St>
       insTAG
-    </>
+    </St>
   );
 }
 
 export default App;
+
+const St = styled.main`
+  color: blue; 
+`;
