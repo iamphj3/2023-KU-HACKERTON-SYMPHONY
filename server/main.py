1c24dbdda639d57ed5e0f5b35c9637aa36deb929
whitepaper907 <101ghgh@naver.com>
2023-08-11 19:55:58 +0900

    test: db 연결

--- server/main.py
+++ server/main.py
@@ -1,16 +1,22 @@
 import uvicorn
 import os
 from fastapi import FastAPI 
-from dotenv import load_dotenv
 from routers import test
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-load_dotenv(os.path.join(BASE_DIR, ".env"))
+from models import mongoDB
 
 app = FastAPI()
 app.include_router(test.router)
- 
 
+# mongodb = mongoDB()
+# @app.on_event("startup")
+# def on_app_start():
+# 	mongodb.connect()
+
+# @app.on_event("shutdown")
+# async def on_app_shutdown():
+# 	mongodb.close()
+    
 @app.get("/")
 def root():
     return {"message": "Hello, insTAG!"}
--- server/routers/test.py
+++ server/routers/test.py
@@ -1,19 +1,31 @@
+from os import environ
 import time
+from dotenv import load_dotenv
 from instagrapi import Client 
 from fastapi import APIRouter
+
+from models import get_db
+
+db = get_db()
 
 router = APIRouter(
 	prefix="/test",
     tags=["test"]
 )
 
+load_dotenv()
+
 cl = Client()
-cl.load_settings('./tmp/dump.json')
-#cl.login(os.environ["ACCOUNT_USERNAME"], os.environ["ACCOUNT_PASSWORD"])
-#cl.login_by_sessionid(os.environ["SESSION_ID"])
+
+#cl.login(environ["ACCOUNT_USERNAME"], environ["ACCOUNT_PASSWORD"])
+#cl.login("insta_gadmin", "ins123tag@!")
 #cl.dump_settings('./tmp/dump.json')
+#cl.login_by_sessionid(os.environ["SESSION_ID"])
 #cl.set_proxy("https://61.37.223.152:8080")
+
+cl.load_settings('./tmp/dump.json')
 cl.get_timeline_feed()
+
 
 @router.get("/list/v1")
 def hashtag_list_v1(tag: str, amount: int):
@@ -28,3 +40,8 @@ def hashtag_list_a1(tag: str, amount: int):
     medias = cl.hashtag_medias_top_a1(tag, amount=amount)
     print("amount"+str(amount)+" timetotal: ", time.time() - start)
     return medias[0].dict()
+
+@router.post("/create")
+async def create_test():
+    result = await db["temp"].insert_one({"test":"test"})
+    print(result)
