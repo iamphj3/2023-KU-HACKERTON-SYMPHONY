5b57ba1330018ec6e9fd3c7418bbf27a659521ec
parkhyeonji <iamphj3@naver.com>
2023-08-12 09:46:53 +0900

    chore : 해시태그 검색 input 스타일링

--- client/src/assets/icons/index.js
+++ client/src/assets/icons/index.js
@@ -1,3 +1,5 @@
 export { ReactComponent as IcLogo } from './ic_logo.svg';
 export { ReactComponent as IcHeart } from './ic_heart.svg';
 export { ReactComponent as IcComment } from './ic_comment.svg';
+export { ReactComponent as IcSearch } from './ic_search.svg';
+export { ReactComponent as IcExpand } from './ic_expand.svg';
--- client/src/components/SearchBox/SearchBox.jsx
+++ client/src/components/SearchBox/SearchBox.jsx
@@ -1,15 +1,25 @@
 import { styled } from 'styled-components';
 import { useState } from 'react';
+import { IcSearch, IcExpand } from '../../assets/icons';
 
 const SEARCH_TABS = ['해시태그 검색', '이미지 검색'];
 
 export default function SearchBox() {
   const [activeTab, setActiveTab] = useState(SEARCH_TABS[0]);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const handleTabClick = (tab) => {
     if (activeTab !== tab) {
       setActiveTab(tab);
     }
+  };
+
+  const handleSearch = () => {
+
+  };
+
+  const handleFilter = () => {
+    setIsExpanded((prev) => !prev);
   };
 
   return (
@@ -25,16 +35,34 @@ export default function SearchBox() {
             {tab}
           </StTabBtn>
         ))}
+        <StInputWrapper>
+          <StSearchInput>
+            <input type="text" placeholder="해시태그를 입력하세요." />
+            <button type="button" onClick={handleSearch}>
+              <IcSearch />
+            </button>
+          </StSearchInput>
+          <div>
+            <p>* 여러개 입력 시 스페이스, 엔터로 구분</p>
+            <StFilterBtn
+              type="button"
+              onClick={handleFilter}
+              className={isExpanded ? 'expanded' : ''}
+              isExpanded={isExpanded}
+            >
+              상세 필터
+              <IcExpand />
+            </StFilterBtn>
+          </div>
+        </StInputWrapper>
       </StTabBar>
-      SearchBox
     </StSearchBox>
   );
 }
 
 const StSearchBox = styled.section`
   width: 100%;
-  height: 15.9rem;
-  margin-bottom: 2.4rem;
+  margin-bottom: 2rem;
 
   border-radius: 1.2rem;
   background-color : ${({ theme }) => theme.colors.White};;
@@ -55,7 +83,59 @@ const StTabBtn = styled.button`
   &:first-child {
     border-radius: 1.2rem 0rem 0rem 0rem;
   }
-  &:last-child {
+  &:nth-child(2) {
     border-radius: 0rem 1.2rem 0rem 0rem;
+  }
+`;
+
+const StInputWrapper = styled.div`
+  padding: 2rem 1.6rem;
+
+  & > div{
+    display: flex;
+    justify-content: space-between;
+
+    & > p {
+      color : ${({ theme }) => theme.colors.Gray5};
+      ${({ theme }) => theme.fonts.Body5};
+    }
+  }
+`;
+
+const StSearchInput = styled.div`
+  position: relative;
+  margin-bottom: 1.35rem;
+
+  & > input {
+    width: 100%;
+    padding: 1.2rem 1.6rem 0.9rem 1.6rem;
+    
+    box-sizing: border-box; // padding이 너비에 영향 미치지 않게
+    border: none;
+    border-radius: 1rem;
+    outline: none;
+    background-color: ${({ theme }) => theme.colors.Gray2};
+    ${({ theme }) => theme.fonts.Body3};
+  }
+
+  & > button {
+    position: absolute;
+    top: 0.9rem;
+    right: 1.6rem;
+  }
+`;
+
+const StFilterBtn = styled.button`
+  display: flex;
+  align-items: center;
+
+
+  & > svg {
+    transform: rotate(360deg); 
+    transition: transform 0.3s ease-in-out; 
+  }
+
+  &.expanded > svg {
+    transform: rotate(180deg); 
   }
 `;
