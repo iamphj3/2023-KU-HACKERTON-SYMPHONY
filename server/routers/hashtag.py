6e8b794c1b9ddd3cba32f4d03093c3da7e125e7a
whitepaper907 <101ghgh@naver.com>
2023-08-17 01:04:07 +0900

    fix : api

--- server/routers/hashtag.py
+++ server/routers/hashtag.py
@@ -1,5 +1,6 @@
 from os import environ
 import time, json, sys, os
+from .url_convert import url_to_base64
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from dotenv import load_dotenv
 from instagrapi import Client
@@ -16,7 +17,8 @@ from time import sleep
 from AI.imageRetrieval import model_predict
 from models.instagrapi_exceptions import handle_exception
 import concurrent.futures
-import asyncio
+import random
+from pydantic import BaseModel
 db = get_db()
 
 router = APIRouter(
@@ -30,18 +32,24 @@ cl.handle_exception = handle_exception
 
 username = environ["PROXY_USER_NAME"]
 pw = environ["PROXY_PASSWORD"]
-proxy = f"https://{username}:{pw}@gate.smartproxy.com:10011"
+port = list()
+for i in range(100):
+    port.append(10000+i)
+proxy = f"https://{username}:{pw}@gate.smartproxy.com:"+str(port[0])
 cl.set_proxy(proxy)
 
 #cl.login(environ["ACCOUNT_USERNAME"], environ["ACCOUNT_PASSWORD"])
 #cl.dump_settings('./tmp/dump.json')
 
 cl.load_settings('./tmp/dump.json')
-cl.get_timeline_feed()
+cl.get_timeline_feed() 
 
-def get_hastag_medias(tag: str, amount:int):
-    print(tag)
-    medias = cl.hashtag_medias_top_v1(tag, amount=amount)
+def get_hashtag_medias(medias):
+    #start = time.time()
+    print("생성")
+    #medias = cl.hashtag_medias_recent_v1(tag, amount=amount)
+    #print("1) tag:"+tag+" "+" amount:"+str(amount)+" timetotal:", time.time() - start) 
+    #start = time.time()
     new_data = []
     for media in medias:
         doc = media.dict()
@@ -69,54 +77,110 @@ def get_hastag_medias(tag: str, amount:int):
                 new_doc["isAds"] = True
                 break
         new_data.append(new_doc)
-        
+
+    #print("2) tag:"+tag+" "+" amount:"+str(amount)+" timetotal:", time.time() - start) 
     return new_data
+
+#def process_function(tag, amount):
+#    return cl.hashtag_medias_recent_v1(tag, amount=amount)
+
+class Hashtag(BaseModel):
+    period : int
+    isAds: bool
+    hashtags : List[str]
+    image_url: Optional[str] = None
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def post_hashtags(hashtags : List[str] = Query(None)):
+async def post_hashtags(hastag : Hashtag):
+    #프록시 설정
+    idx = random.randrange(1,100)
+    new_proxy = f"https://{username}:{pw}@gate.smartproxy.com:"+str(port[idx])
+    cl.set_proxy(new_proxy)
+    start = time.time()
     current_date = datetime.today()
 
     #top 기록 
-    for tag in hashtags:
+    for tag in hastag.hashtags: 
         await db["top"].insert_one({"tag" :tag, "date": current_date})
 
     ##tag_id 저장
     #join한 적 있으면 tagsId에서 삭제
-    tag_obj = await find_tag_id(hashtags)
+    tag_obj = await find_tag_id(hastag.hashtags)
     if tag_obj is not None: # tag_obj 삭제
         await db["tagsId"].find_one_and_delete({"_id" : ObjectId(tag_obj.get("_id"))})
     
-    hashtags.sort()
-    result = await db["tagsId"].insert_one({"tags":hashtags, 
-                                "isLast":False, "isLike":False, "isComment":False}) 
+    hastag.hashtags.sort()
+    if hastag.image_url is not None:
+        result = await db["tagsId"].insert_one({"tags": hastag.hashtags, 
+                                    "isLast":False, "isLike":False, "isComment":False, "isImage":True}) 
+    else: 
+        result = await db["tagsId"].insert_one({"tags": hastag.hashtags, 
+                                    "isLast":False, "isLike":False, "isComment":False, "isImage":False}) 
     tag_id = str(result.inserted_id)
 
     #검색
-    amount = 20
+    fix_amount = 50
+    amount = (int)(fix_amount/len(hastag.hashtags))
+    if(amount<20): amount = 20
+    print(amount)
     collection_names = await db.list_collection_names()
-
+    
     results = []
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        for tag in hashtags:
-            if not(tag in collection_names):
-                future = executor.submit(get_hastag_medias, tag, amount)
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for tag in hastag.hashtags:
+            if not(tag in collection_names):  # 중복 확인
+                future = executor.submit(cl.hashtag_medias_recent_v1, tag, amount)
                 results.append(future)
 
-    for tag, future in zip(hashtags, results):
+    ##멀티쓰레드 
+    # with concurrent.futures.ThreadPoolExecutor() as executor:
+    #     for tag in hastag.hashtags:
+    #         if not(tag in collection_names): # 중복 확인
+    #             future = executor.submit(get_hashtag_medias, tag, amount)
+    #             results.append(future)
+
+    for tag, future in zip(hastag.hashtags, results):
         if future.done():
             result = future.result()
-            if len(result)!=0:
-                await db[tag].insert_many(result)
+            data = get_hashtag_medias(result)
+            if len(data)!=0:
+                await db[tag].insert_many(data)
+
 
     #조인 테이블 생성
-    await tags_union(tag_id, hashtags)
+    await tags_union(tag_id, hastag.hashtags)
+
+    
+    if(hastag.isAds): #광고제거
+        await db[tag_id].delete_many({"isAds": True})
+    if(hastag.period>0): #기간필터
+        current_date = datetime.now()
+        days_ago = current_date - timedelta(days=hastag.period)
+        print(days_ago)
+        await db[tag_id].delete_many({"date": {
+            "$lte": days_ago
+        }})
+    
+    #AI 모델 
+    if hastag.image_url is not None :
+        cursor = db[tag_id].find()
+        docs = await cursor.to_list(length=None)
+        img_docs = list()
+        for doc in docs:
+            if doc["image_url"] is not None:
+                img_docs.append({"id":str(doc["_id"]), "image_url":doc["image_url"]})
+        sort_images = model_predict(hastag.image_url, img_docs)
+        for idx, sort in enumerate(sort_images):
+            await db[tag_id].find_one_and_update({"_id":ObjectId(sort["id"])},{"$set":{"image_rank":idx}})
+    
+    print("total time// amount:"+str(amount)+" timetotal:", time.time() - start)
     return {"status": 201, "message": "검색 종료"}
+
 
 #
 ##collection 합집합 적용 
 async def tags_union(tag_id:str, hashtags : List[str] = Query(None)):
-
     pipeline = []
     stand_tag = None
     collection_names = await db.list_collection_names()
@@ -132,6 +196,7 @@ async def tags_union(tag_id:str, hashtags : List[str] = Query(None)):
                 })
 
     pipeline.append({"$out":tag_id})
+
     if stand_tag is not None:
         cursor = db[stand_tag].aggregate(pipeline)
         await cursor.to_list(length=None)
@@ -155,57 +220,35 @@ async def tags_union(tag_id:str, hashtags : List[str] = Query(None)):
             duplicate_ids = doc["duplicates"]
             del duplicate_ids[0]
             db[tag_id].delete_many({"_id": {"$in": duplicate_ids}})
+    
         
     await update_sort(tag_id, True, False, False)
  
  #
  ##[GET] 해시태그 검색 결과 
 @router.get("/")
-async def get_hashtags(tag_id:str, lastId:str, period:int, isAds:bool, image_url: Optional[str] = None):
+async def get_hashtags(tag_id:str, lastId:str):
     amount = 10
     res = {}
     query = {}
-    #query["_id"] = { "$gt": ObjectId(lastId) }
-
-    #기간 필터 
-    current_date = datetime.now()
-    days_ago = current_date - timedelta(days=period)
-    if period !=0:
-        query["date"] = {
-            "$gte": days_ago,
-            "$lte": current_date
-        }   
-
-    #광고 제거 
-    if isAds:
-        query["isAds"] = {"$ne":True}
-    
-    # #AI 모델 
-
-    if lastId=="000000000000000000000000" and image_url is not None :
-        cursor = db[tag_id].find()
-        docs = await cursor.to_list(length=None)
-        img_docs = list()
-        for doc in docs:
-            if doc["image_url"] is not None:
-                img_docs.append({"id":str(doc["_id"]), "image_url":doc["image_url"]})
-        sort_images = model_predict(image_url, img_docs)
-        for idx, sort in enumerate(sort_images):
-            await db[tag_id].find_one_and_update({"_id":ObjectId(sort["id"])},{"$set":{"image_rank":idx}})
 
     #정렬
     sort_op = []
-    if image_url is not None:
+    sort_doc = await db["tagsId"].find_one({"_id":ObjectId(tag_id)})
+    if sort_doc["isImage"]:
         sort_op.append(('image_rank', 1))
         if(lastId!="000000000000000000000000"):
             last_id = await db[tag_id].find_one({"_id":ObjectId(lastId)})
+            if last_id is None :
+                raise HTTPException(status_code=400, detail={"status":400, "message":"잘못된 lastId 전달"})
             query["image_rank"] = { "$gt": last_id["image_rank"] }
     else:
-        sort_doc = await db["tagsId"].find_one({"_id":ObjectId(tag_id)})
         if sort_doc["isLast"]+sort_doc["isLike"]+sort_doc["isComment"] == 1:
             sort_op.append(('sort_rank', 1))
             if(lastId!="000000000000000000000000"):
                 last_id = await db[tag_id].find_one({"_id":ObjectId(lastId)})
+                if last_id is None :
+                    raise HTTPException(status_code=400, detail={"status":400, "message":"잘못된 lastId 전달"})
                 query["sort_rank"] = { "$gt": last_id["sort_rank"] }  
 
     if len(sort_op) == 0:
@@ -218,7 +261,7 @@ async def get_hashtags(tag_id:str, lastId:str, period:int, isAds:bool, image_url
 
     # 마지막 요청인지 
     if(len(docs)==0): 
-        raise HTTPException(status_code=400, detail={"status":400, "message":"해당 조건에 맞는 검색 결과가 없습니다.(마지막 요청입니다.)"})
+        raise HTTPException(status_code=204, detail={"status":204, "message":"해당 조건에 맞는 검색 결과가 없습니다.(마지막 요청입니다.)"})
     
 
     query_id = {}
@@ -249,7 +292,7 @@ async def get_tag_id(hashtags : List[str] = Query(None)):
     tag_obj = await find_tag_id(hashtags)
 
     if tag_obj is None: #검색결과 없음 return 
-        raise HTTPException(status_code=400, detail={"status":400, "message":"검색 결과가 존재하지 않습니다."})
+        raise HTTPException(status_code=204, detail={"status":204, "message":"검색 결과가 존재하지 않습니다."})
     
     return {"status":200, "message": "tag id return 성공", "data": {"tag_id" : str(tag_obj.get("_id"))}}
 
@@ -321,7 +364,6 @@ async def get_top(period : int):
 
     cursor = db["top"].aggregate(pipeline)
     docs = await cursor.to_list(length=10)
-    print(docs)
     result = list()
     for doc in docs:
         result.append(doc.get('_id'))
