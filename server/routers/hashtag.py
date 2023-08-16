04b0953a8993ee2539edc234fd241f917095b5e5
whitepaper907 <50574878+whitepaper907@users.noreply.github.com>
2023-08-16 15:26:51 +0900

    [server/JM] fix API (#49)
    * fix : instagrapi api 호출 병렬처리
    
    * fix : api res 수정 및 none type error 수정
    
    * fix : 해시태그 조합 정렬 default 최신순로 수정

--- server/routers/hashtag.py
+++ server/routers/hashtag.py
@@ -15,6 +15,8 @@ from urllib3.util.retry import Retry
 from time import sleep
 from AI.imageRetrieval import model_predict
 from models.instagrapi_exceptions import handle_exception
+import concurrent.futures
+import asyncio
 db = get_db()
 
 router = APIRouter(
@@ -28,7 +30,7 @@ cl.handle_exception = handle_exception
 
 username = environ["PROXY_USER_NAME"]
 pw = environ["PROXY_PASSWORD"]
-proxy = f"https://{username}:{pw}@gate.smartproxy.com:10002"
+proxy = f"https://{username}:{pw}@gate.smartproxy.com:10011"
 cl.set_proxy(proxy)
 
 #cl.login(environ["ACCOUNT_USERNAME"], environ["ACCOUNT_PASSWORD"])
@@ -37,92 +39,127 @@ cl.set_proxy(proxy)
 cl.load_settings('./tmp/dump.json')
 cl.get_timeline_feed()
 
+def get_hastag_medias(tag: str, amount:int):
+    print(tag)
+    medias = cl.hashtag_medias_top_v1(tag, amount=amount)
+    new_data = []
+    for media in medias:
+        doc = media.dict()
+        new_doc = {}
+        new_doc["pk"] = str(doc["pk"])
+        new_doc["id"] = str(doc["id"])
+        new_doc["date"] = doc["taken_at"]
+        new_doc["user_name"] = doc["user"]["username"]
+        if doc["thumbnail_url"] is not None :
+            new_doc["image_url"] = doc["thumbnail_url"]
+        else:
+            new_doc["image_url"] = doc["resources"][0]["thumbnail_url"]
+        new_doc["text"] = doc["caption_text"]
+        new_doc["like_count"] = doc["like_count"]
+        new_doc["comment_count"] = doc["comment_count"]
+        new_doc["instagram_url"] = "https://www.instagram.com/p/"+doc["code"]
+        new_doc["isAds"] = False
+        new_doc["image_rank"] = 0
+        new_doc["sort_rank"] = 0
+
+        #광고 여부 
+        check_ads = ['광고', '협찬', '공구', '리그램']
+        for ads in check_ads:
+            if ads in doc["caption_text"]:
+                new_doc["isAds"] = True
+                break
+        new_data.append(new_doc)
+        
+    return new_data
+
+
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def post_hashtags(hashtags : List[str] = Query(None)):
     current_date = datetime.today()
 
     #top 기록 
     for tag in hashtags:
-        #await db["top"].find_one_and_update({"tag": tag}, {'$inc':{"cnt":1}}, upsert=True)
         await db["top"].insert_one({"tag" :tag, "date": current_date})
-    #tag_id 저장
+
+    ##tag_id 저장
+    #join한 적 있으면 tagsId에서 삭제
+    tag_obj = await find_tag_id(hashtags)
+    if tag_obj is not None: # tag_obj 삭제
+        await db["tagsId"].find_one_and_delete({"_id" : ObjectId(tag_obj.get("_id"))})
+    
     hashtags.sort()
     result = await db["tagsId"].insert_one({"tags":hashtags, 
                                 "isLast":False, "isLike":False, "isComment":False}) 
     tag_id = str(result.inserted_id)
 
+    #검색
     amount = 20
     collection_names = await db.list_collection_names()
-    for tag in hashtags:
-        #검색량 저장
-        medias = cl.hashtag_medias_top_v1(tag, amount=amount)
-        if not(tag in collection_names): #검색 기록 확인
-            new_data = []
-            for media in medias:
-                doc = media.dict()
-                new_doc = {}
-                new_doc["pk"] = str(doc["pk"])
-                new_doc["id"] = str(doc["id"])
-                new_doc["date"] = doc["taken_at"]
-                new_doc["user_name"] = doc["user"]["username"]
-                if doc["thumbnail_url"] is not None :
-                    new_doc["image_url"] = doc["thumbnail_url"]
-                else:
-                    new_doc["image_url"] = doc["resources"][0]["thumbnail_url"]
-                new_doc["text"] = doc["caption_text"]
-                new_doc["like_count"] = doc["like_count"]
-                new_doc["comment_count"] = doc["comment_count"]
-                new_doc["isAds"] = False
-                new_doc["image_rank"] = 0
-                new_doc["sort_rank"] = 0
 
-                #광고 여부 
-                check_ads = ['광고', '협찬', '공구']
-                for ads in check_ads:
-                    if ads in doc["caption_text"]:
-                        new_doc["isAds"] = True
-                        break
-                new_data.append(new_doc)
-            result = await db[tag].insert_many(new_data)
+    results = []
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for tag in hashtags:
+            if not(tag in collection_names):
+                future = executor.submit(get_hastag_medias, tag, amount)
+                results.append(future)
+
+    for tag, future in zip(hashtags, results):
+        if future.done():
+            result = future.result()
+            if len(result)!=0:
+                await db[tag].insert_many(result)
 
     #조인 테이블 생성
     await tags_union(tag_id, hashtags)
+    return {"status": 201, "message": "검색 종료"}
 
-#합집합 
+#
+##collection 합집합 적용 
 async def tags_union(tag_id:str, hashtags : List[str] = Query(None)):
-    #collection 합치기
+
     pipeline = []
-    for idx, tag in enumerate(hashtags):
-        if(idx!=0):
-            pipeline.append({
-                "$unionWith":{
-                    "coll": tag
-                }
-            })
+    stand_tag = None
+    collection_names = await db.list_collection_names()
+    for tag in hashtags:
+        if tag in collection_names : # 검색 결과 O
+            if stand_tag is None : # 첫 collection 
+                stand_tag = tag
+            else :
+                pipeline.append({
+                    "$unionWith":{
+                        "coll": tag
+                    }
+                })
+
     pipeline.append({"$out":tag_id})
-    cursor = db[hashtags[0]].aggregate(pipeline)
-    await cursor.to_list(length=None)
+    if stand_tag is not None:
+        cursor = db[stand_tag].aggregate(pipeline)
+        await cursor.to_list(length=None)
 
-    #중복 찾기
-    pipeline = [
-        {
-            "$group": {
-                "_id": "$pk",  # 중복을 확인하려는 필드로 수정
-                "count": {"$sum": 1},
-                "duplicates": {"$addToSet": "$_id"}
-            }
-        },
-        {"$match": {"count": {"$gt": 1}}}
-    ]
-    cursor = db[tag_id].aggregate(pipeline)
-    duplicates = await cursor.to_list(length=None)
+        #중복 찾기
+        pipeline = [
+            {
+                "$group": {
+                    "_id": "$pk",  # 중복을 확인하려는 필드로 수정
+                    "count": {"$sum": 1},
+                    "duplicates": {"$addToSet": "$_id"}
+                }
+            },
+            {"$match": {"count": {"$gt": 1}}}
+        ]
+        cursor = db[tag_id].aggregate(pipeline)
+        duplicates = await cursor.to_list(length=None)
 
-    #중복 제거
-    for doc in duplicates:
-        duplicate_ids = doc["duplicates"]
-        del duplicate_ids[0]
-        db[tag_id].delete_many({"_id": {"$in": duplicate_ids}})
+        #중복 제거
+        for doc in duplicates:
+            duplicate_ids = doc["duplicates"]
+            del duplicate_ids[0]
+            db[tag_id].delete_many({"_id": {"$in": duplicate_ids}})
+        
+    await update_sort(tag_id, True, False, False)
  
+ #
+ ##[GET] 해시태그 검색 결과 
 @router.get("/")
 async def get_hashtags(tag_id:str, lastId:str, period:int, isAds:bool, image_url: Optional[str] = None):
     amount = 10
@@ -181,10 +218,8 @@ async def get_hashtags(tag_id:str, lastId:str, period:int, isAds:bool, image_url
 
     # 마지막 요청인지 
     if(len(docs)==0): 
-        res["isFinal"] = True
-        return {"data" : res} # 결과 X
+        raise HTTPException(status_code=400, detail={"status":400, "message":"해당 조건에 맞는 검색 결과가 없습니다.(마지막 요청입니다.)"})
     
-    res["isFinal"] = False 
 
     query_id = {}
     query_id["_id"] = { "$gt": ObjectId(lastId) }
@@ -195,26 +230,38 @@ async def get_hashtags(tag_id:str, lastId:str, period:int, isAds:bool, image_url
         doc.pop("_id" ,None)
 
     res["results"] = docs
-    return {"data" : res}
+    return {"message": "조회 성공","data" : res}
 
-@router.get("/id")
-async def get_tag_id(hashtags : List[str] = Query(None)):
+#
+##해시태그 조합 검색
+async def find_tag_id(hashtags : List[str] = Query(None)): #해시태그 조합 검색
     find_tags = list()
     for tag in hashtags:
         find_tags.append(tag)
     find_tags.sort()
 
-    tag_obj = await db["tagsId"].find_one({"tags":find_tags})
-    return {"data": {"tag_id" : str(tag_obj.get("_id"))}}
+    return await db["tagsId"].find_one({"tags":find_tags})
 
+#
+##[GET] 해시태그 조합 tag_id get
+@router.get("/id")
+async def get_tag_id(hashtags : List[str] = Query(None)): 
+    tag_obj = await find_tag_id(hashtags)
+
+    if tag_obj is None: #검색결과 없음 return 
+        raise HTTPException(status_code=400, detail={"status":400, "message":"검색 결과가 존재하지 않습니다."})
+    
+    return {"status":200, "message": "tag id return 성공", "data": {"tag_id" : str(tag_obj.get("_id"))}}
+
+#
+##[POST] tag_id collection sort 
 @router.post("/sort", status_code=status.HTTP_201_CREATED)
 async def update_sort(tag_id:str, isLast:bool, isLike:bool, isComment:bool):
     if (isLast + isLike + isComment) > 1: 
-        raise HTTPException(status_code=400, detail="하나의 값만 true로 설정할 수 있습니다.")
+        raise HTTPException(status_code=400, detail={"status":400, "message":"하나의 값만 true로 설정할 수 있습니다."})
 
     sort_op = []
-
-    #인덱스 
+    #정렬
     if(isLike):
         await db[tag_id].create_index([('like_count', -1)])
         sort_op.append(('like_count', -1))
@@ -233,16 +280,22 @@ async def update_sort(tag_id:str, isLast:bool, isLike:bool, isComment:bool):
             await db[tag_id].find_one_and_update({"_id":ObjectId(doc["_id"])},{"$set":{"sort_rank":idx}})
                 
     await db["tagsId"].find_one_and_update({"_id":ObjectId(tag_id)},{"$set":{"isLast":isLast,"isLike":isLike,"isComment":isComment}})
+
+    return {"status":200, "message": "테이블 정렬 완료 : get으로 다시 요청 해주세요"}
      
 @router.get("/total")
 async def get_total(tag_id:str):
     total = await db[tag_id].count_documents({})
-    return {"data" : total}
+    return {"message": "tag_id: "+tag_id + " 게시물 개수 return 성공","data" : total}
 
 
 @router.get("/top")
 async def get_top(period : int):
     query = {}
+    if period != 1 and period !=7 :
+        raise HTTPException(status_code=400, detail={"status":400, "message":"period는 1과 7중에 선택해 전달해주세요."})
+    
+
     #기간 정렬
     current_date = datetime.today()
     days_ago = current_date - timedelta(days=period)
@@ -272,4 +325,4 @@ async def get_top(period : int):
     result = list()
     for doc in docs:
         result.append(doc.get('_id'))
-    return {"data" : result}
+    return {"status":200, "message": "top 10 list return 성공", "data" : result}
