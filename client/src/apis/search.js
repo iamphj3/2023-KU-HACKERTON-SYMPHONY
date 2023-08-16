89f987e7b9f357d74e83dd6a7c2327880d30a27d
parkhyeonji <iamphj3@naver.com>
2023-08-16 19:29:20 +0900

    chore : postSearch request body로 수정

--- client/src/apis/search.js
+++ client/src/apis/search.js
@@ -1,11 +1,28 @@
 import { client } from './axios';
 
 // 해시태그, 이미지 검색 API
-export const postSearch = async (hashtags) => {
+export const postSearch = async (period, isAds, hashtags, image_url) => {
   try {
     console.log('해시태그, 이미지 검색 API 로딩중');
-    const hashtagsQueryParam = hashtags.map((tag) => `hashtags=${tag}`).join('&');
-    const data = await client.post(`/hashtag/?${hashtagsQueryParam}`);
+
+    // const hashtagsQueryParam = hashtags.map((tag) => `hashtags=${tag}`).join('&');
+    // let url = `/hashtag/?period=${period}&isAds=${isAds}&${hashtagsQueryParam}`;
+    // if (image_url) {
+    //   url += `&image_url=${image_url}`;
+    // }
+
+    const requestPayload = {
+      period,
+      isAds,
+      hashtags,
+    };
+
+    if (image_url) {
+      requestPayload.image_url = image_url;
+    }
+
+    const data = await client.post('/hashtag/', requestPayload);
+
     return data.status;
   } catch (err) {
     console.error(err);
